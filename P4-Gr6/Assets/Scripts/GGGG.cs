daab1f8191a784e2720669667e715703cfd17585
Smirnoff-2003 <NE23XN@student.aau.dk>
2026-04-10 11:32:01 +0200

    Zombie trigger script slet

--- P4-Gr6/Assets/Scripts/GGGG.cs
+++ P4-Gr6/Assets/Scripts/GGGG.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class GGGG : MonoBehaviour
 {
+    [SerializeField] private int a;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
