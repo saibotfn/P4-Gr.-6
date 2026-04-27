3f8efb523884733a0dd0365462a6050c9f476352
AReallyFatGoat <zachasaks@gmail.com>
2026-04-27 13:12:17 +0200

    progress on hp

--- P4-Gr6/Assets/Scripts/ScoreManager.cs
+++ P4-Gr6/Assets/Scripts/ScoreManager.cs
@@ -6,6 +6,11 @@ public class ScoreManager : MonoBehaviour
     public int hits = 0;
     public int miss = 0;
     public int score = 0;
+    public int hp = 50;
+    public int maxHP = 50;
+    public int minHP = 0;
+    public int minusHP = 1;
+    public int plusHP = 1;
 
     [SerializeField] private TMP_Text HitsText;
     [SerializeField] private TMP_Text MissText;
@@ -36,5 +41,25 @@ public class ScoreManager : MonoBehaviour
     {
         score += value;
         ScoreText.text = "Score = " + miss.ToString();
+    }
+
+    public void RemoveHP()
+    {
+        plusHP = 1;
+        if (hp >= minHP)
+        {
+            hp = hp - minusHP;
+        }
+        minusHP++;
+    }
+
+    public void AddHP()
+    {
+        minusHP = 1;
+        if(hp <= maxHP)
+          {
+            hp = hp + plusHP;
+          }
+        plusHP++;
     }
 }
