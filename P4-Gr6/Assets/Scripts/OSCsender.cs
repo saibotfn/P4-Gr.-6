2e90f951bc15c75f812951e95cb891368ccc1431
AReallyFatGoat <zachasaks@gmail.com>
2026-05-19 10:34:54 +0200

    OSC sender shit

--- P4-Gr6/Assets/Scripts/OSCsender.cs
+++ P4-Gr6/Assets/Scripts/OSCsender.cs
@@ -1,22 +1,17 @@
 using UnityEngine;
 using OscJack;
-
 public class OSCsender : MonoBehaviour
 {
     OscClient client;
-
     [SerializeField] private ScoreManager scoreManager;
 
     void Start()
     {
         client = new OscClient("127.0.0.1", 9000);
-
-        //InvokeRepeating(nameof(SendAudioData), 0f, 0.05f);
     }
 
    public void SendAudioDataHit()
     {
-
         client.Send("/hitstreak", 1);
         Debug.Log("Hit send");
     }
